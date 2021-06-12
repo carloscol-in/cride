d0386795412afed230a6d408d76e74713103de26
ccolin <contact@carloscolin.me>
2021-06-12 00:22:53 -0500

    added RelatedToCircle viewset to inherit in all viewsets that need to validate a circle exists

---
+++ cride/utils/views.py
@@ -0,0 +1,24 @@
+"""Rides mixins."""
+
+# DRF
+from rest_framework import mixins
+from rest_framework.generics import get_object_or_404
+
+# Models
+from cride.circles.models import Circle
+
+class RelatedToCircle(mixins.GenericViewSet):
+    """This class has to be inherited by all classes that need to
+    dispatch circle objects related to their class."""
+
+    def dispatch(self, request, *args, **kwargs):
+        """Verify that the circle exists."""
+
+        slug_name = kwargs['slug_name']
+
+        self.circle = get_object_or_404(
+            Circle,
+            slug_name=slug_name,
+        )
+
+        return super(RelatedToCircle, self).dispatch(request, *args, **kwargs)
