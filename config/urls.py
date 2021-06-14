92e14b5d439f2e51122f0b64f55b61cf239b310c
ccolin <contact@carloscolin.me>
2021-06-14 00:41:27 -0500

    added rides urls

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
 
     # Circle urls
     path('', include(('cride.circles.urls', 'circles')), name='circles'),
+    path('', include(('cride.rides.urls', 'rides')), name='rides'),
     path('', include(('cride.users.urls', 'users')), name='users'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
