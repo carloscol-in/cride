124a4630273f228eea2c8ed142e1e63d97c424c3
ccolin <contact@carloscolin.me>
2021-06-29 21:55:31 -0500

    added admin actions to verify and unverify circles, and download reports of rides generated the day it's requested

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -1,9 +1,16 @@
 """Circles admin."""
 
+# Python
+import csv
+from datetime import timedelta, datetime
+
 # Django 
 from django.contrib import admin
+from django.utils import timezone
+from django.http import HttpResponse
 
 # Models
+from cride.rides.models import Ride
 from cride.circles.models import Circle
 
 @admin.register(Circle)
@@ -24,3 +31,54 @@ class CircleAdmin(admin.ModelAdmin):
         'verified',
         'is_limited',
     )
+
+    actions = ['unverify_circles', 'verify_circles', 'download_todays_rides']
+
+    def unverify_circles(self, request, queryset):
+        """Make circles verified."""
+        queryset.update(verified=False)
+    unverify_circles.short_description = 'Make selected circles not verified'
+
+    def verify_circles(self, request, queryset):
+        """Make circles verified."""
+        queryset.update(verified=True)
+    verify_circles.short_description = 'Make selected circles verified'
+
+    def download_todays_rides(self, request, queryset):
+        """Return today's rides."""
+        now = timezone.now()
+        start = datetime(now.year, now.month, now.day, 0, 0, 0)
+        end = start + timedelta(days=1)
+        rides_today = Ride.objects.filter(
+            offered_in__in=queryset.values_list('id'),
+            departure_date__gte=start,
+            departure_date__lte=end
+        ).order_by('departure_date')
+
+        # import pdb;pdb.set_trace()
+
+        response = HttpResponse(content_type='text/csv')
+
+        if len(queryset) == 1:
+            response['Content-Disposition'] = f'attachment; filename="{start.strftime("%Y-%m-%d")}_rides--{queryset[0].slug_name}.csv"'
+        elif len(queryset) > 1:
+            response['Content-Disposition'] = f'attachment; filename="{start.strftime("%Y-%m-%d")}_rides.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow([
+            'id', 'passengers', 'departure_location',
+            'departure_date', 'arrival_location',
+            'arrival_date', 'rating',
+        ])
+        for ride in rides_today:
+            writer.writerow([
+                ride.pk,
+                ride.passengers.count(),
+                ride.departure_location,
+                str(ride.departure_date),
+                ride.arrival_location,
+                str(ride.arrival_date),
+                ride.rating,
+            ])
+        return response
+    download_todays_rides.short_description = 'Download today\'s rides in CSV'
